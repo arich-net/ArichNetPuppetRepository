c1f6ccdd0b929ad60f61f2c9fcc33c97c4237abc
Ariel Vasquez <arich.net@gmail.com>
2015-10-19 19:39:26 +0200

    Adding test-host1 and base modeule path changed

--- environments/production/manifests/site.pp
+++ environments/production/manifests/site.pp
@@ -1,5 +1,6 @@
-file {'/tmp/example-ip':                                            # resource type file and filename
-  ensure  => present,                                               # make sure it exists
-  mode    => 0644,                                                  # file permissions
-  content => "ARICH-NET Here is my Public IP Address: ${ipaddress_eth0}.\n",  		    # note the ipaddress_eth0 fact
-}
+# Local imports
+import "baselines/*.pp"
+import "nodes/*.pp"
+import "roles/*.pp"
+import "defines.pp"
+
