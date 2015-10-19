3341ab9a443d3b189ce88900b65f3617375526a0
Ariel Vasquez <arich.net@gmail.com>
2015-10-19 20:41:08 +0200

    Testing SSH Module on Puppet Site

---
+++ Reference/backup_modules/ssh/manifests/init.pp
@@ -0,0 +1,16 @@
+# Class: ssh
+#
+# This module manages ssh
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class ssh() {
+	
+}
--- environments/production/manifests/nodes/test-host1.pp
+++ environments/production/manifests/nodes/test-host1.pp
@@ -1,14 +1,4 @@
 node 'test-host1.arich-net.com' {
-   #
    # SSH
-   #
-   class { 'ssh::server':
-      ssh_usepam => 'yes',
-      ssh_pubkeyauthentication => 'no',
-      ssh_x11forwarding => 'no',
-      ssh_allowtcpforwarding => 'no',
-      ssh_allowagentforwarding => 'no',
-      ssh_clientaliveinterval => '900',
-      ssh_clientalivecountmax => '0',
-   }   
+   include ssh
 }
