f0f6196b1cdf34e0a6ff27cb8112594a87de34b8
Ariel Vasquez <arich.net@gmail.com>
2015-10-19 22:34:13 +0200

    SSH module with banner

--- environments/production/manifests/nodes/test-host1.pp
+++ environments/production/manifests/nodes/test-host1.pp
@@ -3,5 +3,7 @@ node 'test-host1.arich-net.com' {
    class { 'ssh':
       permit_root_login => 'yes',
       sshd_config_allowusers => ['root'],
+      sshd_config_banner => '/etc/banner',
+      sshd_banner_content => 'Welcome to server ${fqdn}',
    }
 }
