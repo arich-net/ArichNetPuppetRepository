9a72e0a06d9e9d658ec08adbad8164dbc6e75067
Ariel Vasquez <arich.net@gmail.com>
2015-10-20 21:10:18 +0200

    Adding arich-digitalocean host on puppet

---
+++ environments/production/manifests/nodes/arich-digitalocean.pp
@@ -0,0 +1,9 @@
+node 'arich-digitalocean.arich-net.com' {
+   # SSH
+   class { 'ssh':
+      permit_root_login => 'no',
+      sshd_config_allowusers => ['root','arich'],
+      sshd_config_banner => '/etc/banner',
+      sshd_banner_content => "Welcome to server ${fqdn}",
+   }
+}
