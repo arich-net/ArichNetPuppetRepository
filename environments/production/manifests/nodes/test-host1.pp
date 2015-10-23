a0ad72c13d7cc2bc6a7f4b0979470c82e7e813c5
arich.net <arich.net@arich-net.com>
2015-10-23 23:45:31 +0200

    Working node with apt

--- environments/production/manifests/nodes/test-host1.pp
+++ environments/production/manifests/nodes/test-host1.pp
@@ -1,7 +1,29 @@
 node 'test-host1.arich-net.com' {
+   #
    # APT
-   include apt
+   #
+   case $lsbdistid {
+      'Ubuntu':	{ $location = 'http://es.archive.ubuntu.com/ubuntu'  }
+      'Debian':	{ $location = 'http://ftp.es.debian.org/debian'  }
+   }
+
+   class { 'apt':
+      purge => {
+         'sources.list' => true,
+      }
+   }
+
+   apt::source { 'ubuntu':
+      location => $location,
+      repos => 'main restricted',
+      include => {
+         'deb' => true,
+      }
+   }
+
+   #
    # SSH
+   #
    class { 'ssh':
       permit_root_login => 'yes',
       sshd_config_allowusers => ['root'],
