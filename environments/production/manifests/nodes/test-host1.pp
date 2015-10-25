5dfe15f73566161d146b9a1f16b099fe4075e2c2
Ariel Vasquez <arich.net@gmail.com>
2015-10-25 18:32:19 +0100

    Adding java installation for logstash module. Adding new ERB files for input and filter

--- environments/production/manifests/nodes/test-host1.pp
+++ environments/production/manifests/nodes/test-host1.pp
@@ -40,14 +40,23 @@ node 'test-host1.arich-net.com' {
    ####################
    #      END APT     #
    ####################
-   class { 'logstash': }
+   class { 'logstash': 
+      java_install => true,  
+   }
 
-   $rabbit_password = hiera("rabbit_password")
-   
+   $rabbit_password = hiera("rabbit_password")   
    logstash::configfile { 'output_$hostname':
       content => template("/opt/puppetmaster/codedir/environments/${environment}/templates/logstash/output_${hostname}.erb"),
+      order   => 30
+   }
+   logstash::configfile { 'input_$hostname':
+      content => template("/opt/puppetmaster/codedir/environments/${environment}/templates/logstash/input_${hostname}.erb"),
       order   => 10
-   }   
+   }
+   logstash::configfile { 'input_$hostname':
+      content => template("/opt/puppetmaster/codedir/environments/${environment}/templates/logstash/filter_${hostname}.erb"),
+      order   => 10
+   }
 
    #
    # SSH
