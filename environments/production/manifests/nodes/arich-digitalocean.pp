cd937ec003057037ba9a7505d5037b56a03ccd8f
Ariel Vasquez <arich.net@gmail.com>
2017-01-07 23:39:04 +0100

    Fixing some issues

--- environments/production/manifests/nodes/arich-digitalocean.pp
+++ environments/production/manifests/nodes/arich-digitalocean.pp
@@ -26,7 +26,7 @@ node 'arich-digitalocean.arich-net.com' {
       sshd_banner_content => "Welcome to server ${fqdn} \n",
       sshd_config_challenge_resp_auth => 'no',
       sshd_password_authentication => 'no',
-      sshd_use_pam_real => 'no',
+      sshd_use_pam => 'no',
    }
 
    # Mcollective
